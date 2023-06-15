65d6247ffde8581554a07cfd2ea2eb0b439b8242
沈银岗 <shenyingang@chuanglintech.com>
2023-06-15 11:33:52 +0800

    Feat：WebGPU代码提交

---
+++ src/layouts/gpu/gpuForce.ts
@@ -0,0 +1,19 @@
+export class FruchtermanGPULayout {
+    public maxIteration: number = 300 // 最大迭代次数
+
+    public gravity: number = 10 // 重力大小
+
+    public speed: number = 1 // 迭代速度
+
+    public nodeMap: {[key:string]: any} = {}
+
+    constructor() {}
+
+    excute = () => {
+
+    }
+
+    run = () => {
+        
+    }
+}
